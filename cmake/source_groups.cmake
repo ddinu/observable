43379deac2fe10381b51a854f416669005b7d65c
Daniel Dinu <daniel@danieldinu.com>
2016-09-02 20:07:20 +0300

    Global cmake source groups configiuration.

---
+++ cmake/source_groups.cmake
@@ -0,0 +1,5 @@
+source_group(include REGULAR_EXPRESSION .*include/.*)
+source_group(include\\detail REGULAR_EXPRESSION .*include/.*/detail/.*)
+source_group(include\\detail\\subject REGULAR_EXPRESSION .*include/.*/detail/subject/.*)
+source_group(src REGULAR_EXPRESSION .*src/.*)
+source_group(src\\detail REGULAR_EXPRESSION .*src/detail/.*)
