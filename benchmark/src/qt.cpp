d851c621617c241a4efdbcc58febb6117a6d5c5e
Daniel Dinu <daniel@danieldinu.com>
2016-11-18 00:28:23 +0200

    Improve the Qt benchmark.

--- benchmark/src/qt.cpp
+++ benchmark/src/qt.cpp
@@ -4,32 +4,38 @@
 #include <iostream>
 #include <observable/subject.hpp>
 
-static auto const repeat_count = 1'000'000;
+static auto const repeat_count = 100'000;
+static auto const sub_count = 100;
 
 void bench()
 {
     Sender sender;
     Receiver receiver;
 
-    auto success = receiver.connect(&sender, &Sender::inc,
-                                    &receiver, &Receiver::inc,
-                                    Qt::DirectConnection);
-    (void)success;
-    assert(success);
+    for(auto i = 0; i < sub_count; ++i)
+    {
+        auto success = receiver.connect(&sender, &Sender::inc,
+                                        &receiver, &Receiver::inc,
+                                        Qt::DirectConnection);
+        (void)success;
+        assert(success);
+    }
 
     auto qt_duration = benchmark::time_run([&]() { emit sender.inc(1); },
                                            repeat_count);
 
-    assert(receiver.dummy == repeat_count);
+    assert(receiver.dummy == repeat_count * sub_count);
     receiver.dummy = 0;
 
     observable::subject<void(int)> subject;
-    subject.subscribe([&](int v) { receiver.inc(v); }).release();
+
+    for(auto i = 0; i < sub_count; ++i)
+        subject.subscribe([&](int v) { receiver.inc(v); }).release();
 
     auto subject_duration = benchmark::time_run([&]() { subject.notify(1); },
                                                 repeat_count);
 
-    assert(receiver.dummy == repeat_count);
+    assert(receiver.dummy == repeat_count * sub_count);
     receiver.dummy = 0;
 
     benchmark::print("Qt signal-slot", qt_duration, "Subject", subject_duration);
