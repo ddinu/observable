8f326474e4aeb5480ac006aded9926937f6ae4e6
Daniel Dinu <daniel@danieldinu.com>
2017-01-07 01:33:08 +0200

    Expression tree. This is the first step towards observable expressions.

---
+++ include/observable/detail/expression_tree.hpp
@@ -0,0 +1,200 @@
+#pragma once
+#include <functional>
+#include <memory>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+#include "observable/subject.hpp"
+#include "observable/subscription.hpp"
+#include "observable/value.hpp"
+
+namespace observable { namespace detail {
+
+//! An expression tree can evaluate an arbitrary expression.
+//!
+//! The term expression, is intentionally vague as it can be anything. Expressions
+//! are formed from n-ary user-supplied operators and operands that can be
+//! constants, observable values or other nodes.
+//!
+//! The tree will propagate change notifications upwards, so any change
+//! to any value<> contained in the expression will be propagated to the root
+//! node.
+//!
+//! When evaluating the root node, only nodes that have been changed will be
+//! evaluated.
+//!
+//! \warning None of the class' methods can be safely called concurrently.
+template <typename ResultType>
+class expression_node
+{
+public:
+    //! Create a new node from a constant value.
+    //!
+    //! \param[in] constant A constant value that will become the node's evaluated
+    //!                     value.
+    //!
+    //! Nodes created with this constructor are called constant nodes. These nodes
+    //! never notify their subscribers of value changes.
+    template <typename ValueType>
+    explicit expression_node(ValueType && constant)
+    {
+        static_assert(std::is_convertible<ValueType, ResultType>::value,
+                      "ValueType must be convertible to ResultType.");
+
+        data_->result = std::forward<ValueType>(constant);
+        data_->dirty = false;
+        data_->eval = []() { };
+    }
+
+    //! Create a new node from an observable value.
+    //!
+    //! \param[in] value An observable value who will become the node's evaluated
+    //!                  value. This value does not need to be kept alive for any
+    //!                  length of time.
+    //!
+    //! Nodes created with this constructor are called value nodes. These nodes
+    //! notify their subscribers of value changes as long as the value is alive.
+    template <typename ValueType>
+    explicit expression_node(value<ValueType> & value)
+    {
+        static_assert(std::is_convertible<ValueType, ResultType>::value,
+                      "ValueType must be convertible to ResultType.");
+
+        data_->subs.emplace_back(value.subscribe([d = data_.get()]() {
+                                                      d->dirty = true;
+                                                      d->notify();
+                                                  }));
+
+        data_->eval = [v = &value, d = data_.get()]() {
+                          if(!d->dirty)
+                              return;
+
+                          d->result = v->get();
+                          d->dirty = false;
+                      };
+
+        data_->eval(); // So we cache the value in case the observable value dies
+                       // before our node's first eval.
+    }
+
+    //! Create a new node from a number of nodes and an n-ary operation.
+    //!
+    //! \param[in] op An n-ary operation with the signature below:
+    //!
+    //!                     ResultType (ValueType const & ...)
+    //!
+    //! \param[in] nodes Expression nodes who's value will be the operand to the
+    //!                  operation.
+    //!
+    //! Nodes created with this constructor are called unary nodes. These nodes
+    //! notify their subscribers of value changes.
+    template <typename OpType, typename ... ValueType>
+    expression_node(OpType && op, expression_node<ValueType> && ... nodes)
+    {
+        static_assert(std::is_convertible<decltype(op(ValueType {} ...)), ResultType>::value,
+                      "Operation must return a type that is convertible to ResultType.");
+
+        subscribe_to_nodes(nodes ...);
+
+        data_->eval = [t = std::make_tuple(std::move(nodes) ...),
+                       o= std::forward<OpType>(op),
+                       d = data_.get()]() mutable {
+                          if(!d->dirty)
+                              return;
+
+                          constexpr auto const size = std::tuple_size<decltype(t)>::value;
+                          constexpr auto const indices = std::make_index_sequence<size> { };
+
+                          d->result = call_with_tuple(o, t, indices);
+                          d->dirty = false;
+                      };
+
+        data_->eval();
+    }
+
+    //! Execute the stored operation and update the node's result value.
+    auto eval() const { data_->eval(); }
+
+    //! Retrieve the expression node's result value. This will not evaluate the
+    //! node so this value might be stale.
+    auto get() const { return data_->result; }
+
+    //! Subscribe to change notifications from this node.
+    template <typename Observer>
+    auto subscribe(Observer && callable) { return data_->subscribe(callable); }
+
+public:
+    //! Expression nodes are not default-constructible.
+    expression_node() =default;
+
+    //! Expression nodes are copy-constructible.
+    //!
+    //! \warning expression_nodes use shallow copies.
+    expression_node(expression_node const &) =default;
+
+    //! Expression nodes are copy-assignable.
+    //!
+    //! \warning expression_nodes use shallow copies.
+    auto operator=(expression_node const &) -> expression_node & =default;
+
+    //! Expression nodes are move-constructible.
+    expression_node(expression_node && other) noexcept =default;
+
+    //! Expression nodes are move-assignable.
+    auto operator=(expression_node && other) noexcept -> expression_node & =default;
+
+private:
+    template <typename Head, typename ... Nodes>
+    void subscribe_to_nodes(Head & head, Nodes & ... nodes)
+    {
+        data_->subs.emplace_back(
+                    head.subscribe([d = data_.get()]() {
+                                    d->dirty = true;
+                                        d->notify();
+                                    }));
+
+        subscribe_to_nodes(nodes ...);
+    }
+
+    template <typename ... Nodes>
+    void subscribe_to_nodes()
+    {
+        // Do nothing.
+    }
+
+    template <typename Tuple, std::size_t I=0>
+    static auto eval_tuple(Tuple & nodes) ->
+        typename std::enable_if<I < std::tuple_size<Tuple>::value, void>::type
+    {
+        std::get<I>(nodes).eval();
+        eval_tuple<Tuple, I + 1>(nodes);
+    }
+
+    template <typename Tuple, std::size_t I=0>
+    static auto eval_tuple(Tuple & nodes) ->
+        typename std::enable_if<I >= std::tuple_size<Tuple>::value, void>::type
+    {
+        // Do nothing.
+    }
+
+    template <typename Fun, typename Tuple, std::size_t ... I>
+    static auto call_with_tuple(Fun & fun, Tuple & nodes, std::index_sequence<I ...>)
+    {
+        eval_tuple(nodes);
+        return fun(std::get<I>(nodes).get() ...);
+    }
+
+private:
+    struct data : subject<void()>
+    {
+        ResultType result;
+        bool dirty = true;
+        std::function<void()> eval;
+        std::vector<unique_subscription> subs;
+    };
+
+    std::shared_ptr<data> data_ { std::make_shared<data>() };
+};
+
+} }
---
+++ tests/src/detail/expression_tree.cpp
@@ -0,0 +1,250 @@
+#include "observable/value.hpp"
+#include "observable/detail/expression_tree.hpp"
+#include "gtest.h"
+
+namespace observable { namespace detail { namespace test {
+
+TEST(expression_tree_test, constant_node_can_be_evaluated)
+{
+    auto node = expression_node<int> { 5 };
+    node.eval();
+
+    ASSERT_EQ(5, node.get());
+}
+
+TEST(expression_tree_test, constant_node_can_be_evaluated_after_move)
+{
+    auto node = expression_node<int> { 5 };
+    auto new_node = std::move(node);
+    new_node.eval();
+
+    ASSERT_EQ(5, new_node.get());
+}
+
+TEST(expression_tree_test, value_node_can_be_evaluated)
+{
+    auto val = value<int> { 5 };
+    auto node = expression_node<int> { val };
+    node.eval();
+
+    ASSERT_EQ(5, node.get());
+}
+
+TEST(expression_tree_test, value_node_can_be_evaluated_after_move)
+{
+    auto val = value<int> { 5 };
+    auto node = expression_node<int> { val };
+    auto new_node = std::move(node);
+    new_node.eval();
+
+    ASSERT_EQ(5, new_node.get());
+}
+
+TEST(expression_tree_test, value_node_can_be_evaluated_after_value_is_dead)
+{
+    auto node = expression_node<int> { };
+
+    {
+        auto val = value<int> { 5 };
+        node = expression_node<int> { val };
+    }
+
+    node.eval();
+
+    ASSERT_EQ(5, node.get());
+}
+
+TEST(expression_tree_test, change_is_reflected_in_value_node_after_eval)
+{
+    auto val = value<int> { 5 };
+    auto node = expression_node<int> { val };
+    val = 7;
+    node.eval();
+
+    ASSERT_EQ(7, node.get());
+}
+
+TEST(expression_tree_test, change_is_not_reflected_in_value_node_without_eval)
+{
+    auto val = value<int> { 5 };
+    auto node = expression_node<int> { val };
+    val = 7;
+
+    ASSERT_EQ(5, node.get());
+}
+
+TEST(expression_tree_test, change_is_reflected_in_value_node_after_move)
+{
+    auto val = value<int> { 5 };
+    auto node = expression_node<int> { val };
+    auto new_node = std::move(node);
+    val = 7;
+    new_node.eval();
+
+    ASSERT_EQ(7, new_node.get());
+}
+
+TEST(expression_tree_test, change_triggers_value_node_notificaction)
+{
+    auto val = value<int> { 5 };
+    auto node = expression_node<int> { val };
+
+    auto called = false;
+    node.subscribe([&]() { called = true; }).release();
+    val = 7;
+
+    ASSERT_TRUE(called);
+}
+
+TEST(expression_tree_test, change_triggers_value_node_notification_after_move)
+{
+    auto val = value<int> { 5 };
+    auto node = expression_node<int> { val };
+
+    auto called = false;
+    node.subscribe([&]() { called = true; }).release();
+
+    auto new_node = std::move(node);
+    val = 7;
+
+    ASSERT_TRUE(called);
+}
+
+TEST(expression_tree_test, unary_node_can_be_evaluated)
+{
+    auto node = expression_node<int> {
+        [](auto v) { return v * 2; },
+        expression_node<int> { 5 }
+    };
+    node.eval();
+
+    ASSERT_EQ(5 * 2, node.get());
+}
+
+TEST(expression_tree_test, unary_node_can_be_evaluated_after_move)
+{
+    auto node = expression_node<int> {
+        [](auto v) { return v * 2; },
+        expression_node<int> { 5 }
+    };
+
+    auto new_node = std::move(node);
+    new_node.eval();
+
+    ASSERT_EQ(5 * 2, new_node.get());
+}
+
+TEST(expression_tree_test, change_is_reflected_in_unary_node)
+{
+    auto val = value<int> { 5 };
+    auto node = expression_node<int> {
+        [](auto v) { return v * 2; },
+        expression_node<int> { val }
+    };
+    val = 7;
+    node.eval();
+
+    ASSERT_EQ(7 * 2, node.get());
+}
+
+TEST(expression_tree_test, change_is_reflected_in_unary_node_after_move)
+{
+    auto val = value<int> { 5 };
+    auto node = expression_node<int> {
+        [](auto v) { return v * 2; },
+        expression_node<int> { val }
+    };
+
+    auto new_node = std::move(node);
+    val = 7;
+    new_node.eval();
+
+    ASSERT_EQ(7 * 2, new_node.get());
+}
+
+TEST(expression_tree_test, unary_node_only_evaluates_op_if_dirty)
+{
+    auto call_count = 0;
+    auto node = expression_node<int> {
+        [&](auto val) { ++call_count; return val; },
+        expression_node<int> { 5 }
+    };
+
+    node.eval();
+    node.eval();
+
+    ASSERT_EQ(1, call_count);
+}
+
+TEST(expression_tree_test, binary_node_can_be_evaluated)
+{
+    auto node = expression_node<int> {
+        [](auto a, auto b) { return a + b; },
+        expression_node<int> { 5 },
+        expression_node<int> { 7 }
+    };
+    node.eval();
+
+    ASSERT_EQ(5 + 7, node.get());
+}
+
+TEST(expression_tree_test, binary_node_can_be_evaluated_after_move)
+{
+    auto node = expression_node<int> {
+        [](auto a, auto b) { return a + b; },
+        expression_node<int> { 5 },
+        expression_node<int> { 7 }
+    };
+
+    auto new_node = std::move(node);
+    new_node.eval();
+
+    ASSERT_EQ(5 + 7, new_node.get());
+}
+
+TEST(expression_tree_test, change_is_reflected_in_binary_node)
+{
+    auto val = value<int> { 5 };
+    auto node = expression_node<int> {
+        [](auto a, auto b) { return a + b; },
+        expression_node<int> { val },
+        expression_node<int> { 7 }
+    };
+    val = 3;
+    node.eval();
+
+    ASSERT_EQ(3 + 7, node.get());
+}
+
+TEST(expression_tree_test, change_is_reflected_in_binary_node_after_move)
+{
+    auto val = value<int> { 5 };
+    auto node = expression_node<int> {
+        [](auto a, auto b) { return a + b; },
+        expression_node<int> { val },
+        expression_node<int> { 7 }
+    };
+
+    auto new_node = std::move(node);
+    val = 3;
+    new_node.eval();
+
+    ASSERT_EQ(3 + 7, new_node.get());
+}
+
+TEST(expression_tree_test, binary_node_only_evaluates_op_if_dirty)
+{
+    auto call_count = 0;
+    auto node = expression_node<int> {
+        [&](auto a, auto b) { ++call_count; return a + b; },
+        expression_node<int> { 5 },
+        expression_node<int> { 7 }
+    };
+
+    node.eval();
+    node.eval();
+
+    ASSERT_EQ(1, call_count);
+}
+
+} } }
